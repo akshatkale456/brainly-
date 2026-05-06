553a891cbdaa6085431c62b8f7f41e00ee36743e
akshatkale456 <akshatkale02@gmail.com>
2026-05-06 22:48:48 +0530

    new day

--- src/store.ts/store.ts
+++ src/store.ts/store.ts
@@ -2,40 +2,46 @@ import { create } from "zustand"
 import type { cardGlobalState } from "../types/type"
 import type { card } from "../types/type"
 const useCardset = create<cardGlobalState>((set) => ({
-    card: [],
-    addcard: async (newCard: card) => {
-        const temporaryCard =   {
-            type: newCard.type,
-            title: newCard.title,
-            read: newCard.read,
-            link: newCard.link,
-            priority: newCard.priority || "low",
-            id: new Date().getTime()
-        };
-        
-        set((state) => ({
-            card: [...state.card, temporaryCard]
-        }));
-    },
-    deletcard: async (id: number) => {set((state) => ({
-        card: state.card.filter((card) => card.id !== id)
-    }))
-    await fetch("#",{
+    card :[],
+    
+    addcard:async (newCard:card)=>{
+    const  tempid = Date.now()
+    
+         const temproarycard = {
+            title:newCard.title,
+            type:newCard.type,
+            id: tempid,
+            priority:newCard.priority
 
-    })
-},
-
-    editcard:(id:number,updatedData)=>set((state)=>{
+        }
+        set((state)=>({
+            card:[...state.card,temproarycard]
+        }))
+        const response = await fetch
+    set((state)=>({
         card:state.card.map((c)=>{
-            if ( c.id === id ){
-                return {
+            if(c.id == tempid){
+                return{
                     ...c,
-                    ...updatedData
+                    id:response.id
                 }
             }
-        }
-        )
-    })
-}));
+    else{
+                return c
+            }
+
+        })
+    }))
+
+        
+    },
+    deletcard(id) {
+        
+    },
+    editcard(id, updatedData) {
+        
+    },
+
+}))
 
 export default useCardset;
--- src/store.ts/todostore.ts
+++ src/store.ts/todostore.ts
@@ -2,29 +2,4 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { TodoGlobalState, TodoItem } from "../types/type";
 
-export const useTodoStore = create<TodoGlobalState>()(
-    persist(
-        (set) => ({
-            todos: [],
-            addTodo: (newTodo) => set((state) => {
-                const todo: TodoItem = {
-                    ...newTodo,
-                    id: Date.now(),
-                    complete: false
-                };
-                return { todos: [...state.todos, todo] };
-            }),
-            deleteTodo: (id) => set((state) => ({
-                todos: state.todos.filter((t) => t.id !== id)
-            })),
-            toggleTodoComplete: (id) => set((state) => ({
-                todos: state.todos.map((t) =>
-                    t.id === id ? { ...t, complete: !t.complete } : t
-                )
-            }))
-        }),
-        {
-            name: "todo-storage", // stores tasks in localStorage
-        }
-    )
-);
+export const useTodoStore = c
